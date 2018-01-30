789653d51081e578a64e2240dba1d581cfa30dee
RiCK BROWN <ricky.brown.00@gmail.com>
2018-01-30 00:43:24 +0000

    added popup functionality to the first 2 Group A matches.

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,5 @@
+function popUp(){
+    prompt('Please enter the total game points scored by Player 1:', " ");
+    prompt('Please enter the total game points scored by Player 2:', " ");
+    prompt('Please enter the total game score:', " ");
+}
