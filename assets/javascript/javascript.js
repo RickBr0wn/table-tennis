72267872a3b7f6d377abab391ec27dcabcb144dc
RickBr0wn <ricky.brown.00@gmail.com>
2018-02-03 17:34:57 +0000

    fixed hide/show button

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,11 +1,14 @@
 function hidePlayerData(){
   let div = document.getElementById('player-entry');
+  let hideButtonText = document.getElementById('btn-hidden');
 
   if (div.style.display !== 'none') {
         div.style.display = 'none';
+        hideButtonText.innerHTML = 'Show Player Entry'; 
     }
     else {
         div.style.display = 'block';
+        hideButtonText.innerHTML = 'Hide Player Entry';
     }
 }
 
